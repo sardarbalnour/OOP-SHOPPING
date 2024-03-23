be185d3506b841bf5ae1a9b9312bc9c4adaa9bb4
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-23 10:45:23 +0330

    Products class

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import Products from "./models/Products.js";
 import { fetchData } from "./utils/httpReq.js";
+
+const productsNode = document.querySelector("#products");
 
 const render = async () => {
   const productData = await fetchData();
-  console.log(productData);
+  const productsInstance = new Products(productsNode, productData);
+  productsInstance.showProducts();
 };
 
 document.addEventListener("DOMContentLoaded", render);
